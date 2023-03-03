4cd4cc97cc5d9efdec472a720da65e0d7c28e6b3
Andrius Pranskunas <noriunietis1@gmail.com>
2023-03-03 12:40:32 +0200

    Cart functionality, upgrade2

---
+++ database/migrations/2023_03_02_092618_create_shelf_content_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shelf_content', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('description');
+            $table->string('image')->nullable();
+            $table->integer('category_id');
+            $table->string('author');
+            $table->string('pages');
+            $table->string('price');
+            $table->integer('status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shelf_content');
+    }
+};
